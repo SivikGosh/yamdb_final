f4ddd848d01dc6c7e1ba472e4e5e9482526ddea5
SivikGosh <sivik@protonmail.com>
2023-02-16 15:04:09 +0300

    review2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MMEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # e-mail emulatiom settings
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
